9036debfc70b6f8d50955d9f1aec1c5af72ece9c
aglaya-pill <fedorsov21@mail.ru@>
2019-10-05 13:04:31 +0300

    'rsa'

---
+++ homework01/RSA.py
@@ -0,0 +1,144 @@
+import random
+
+
+def is_prime(n):
+    """
+    Tests to see if a number is prime.
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    # PUT YOUR CODE HERE
+    if n % 2 == 0 and n != 2:
+        return False
+    else:
+        j = 0
+        for i in range(1, n + 1, 2):
+            if n % i == 0:
+                j += 1
+        if j > 2:
+            return False
+        else:
+            return True
+
+    # pass
+
+
+def gcd(a, b):
+    """
+    Euclid's algorithm for determining the greatest common divisor.
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    # PUT YOUR CODE HERE
+    if a < b:
+        a, b = b, a
+    while a % b != 0:
+        c = a % b
+        a = b
+        b = c
+    return b
+    # pass
+
+
+def multiplicative_inverse(b, a):
+    """
+    Euclid's extended algorithm for finding the multiplicative
+    inverse of two numbers.
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    # PUT YOUR CODE HERE
+    j = 0
+    d = []
+    if a < b:
+        a, b = b, a
+    while a % b != 0:
+        j += 1
+        c = a % b
+        d.append(a // b)
+        a = b
+        b = c
+    d1 = d.copy()
+    d1.reverse()
+    x = []
+    y = []
+    x.append(0)
+    y.append(1)
+    for i in range(1, j):
+        x.append(y[i - 1])
+        z = x[i - 1] - y[i - 1] * d1[i - 1]
+        y.append(z)
+    return x[j - 1] - y[j - 1] * d1[j - 1]
+    # pass
+
+
+def generate_keypair(p, q):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+
+    # n = pq
+    # PUT YOUR CODE HERE
+    n= p*q
+
+    # phi = (p-1)(q-1)
+    # PUT YOUR CODE HERE
+    phi= (p-1)*(q-1)
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
+
+
+def encrypt(pk, plaintext):
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return ''.join(plain)
+
+
+if __name__ == '__main__':
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
