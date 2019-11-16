cb6ffa3747fa7c181158ea7554eafcb3ab0af031
aglaya-pill <fedorsov21@mail.ru@>
2019-11-16 13:08:41 +0300

    добавлен эхо-бот

---
+++ homework05/echo_bot.py
@@ -0,0 +1,14 @@
+import telebot
+
+
+access_token = '689014667:AAEbrpA84JGFiy8z47-G0RWgHJJkEHN-bls'
+telebot.apihelper.proxy = {'https': 'https://69.15.2.22:8080'}
+bot = telebot.TeleBot(access_token)
+
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
+
